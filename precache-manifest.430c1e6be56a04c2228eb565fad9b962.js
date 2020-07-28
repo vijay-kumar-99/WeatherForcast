791075fa80e7fa1a8c5f4a9a6eaea23e605278f5
vijay-kumar-99 <vijaykumarsala99@gmail.com>
2020-07-28 16:51:48 +0530

    Updates

---
+++ precache-manifest.430c1e6be56a04c2228eb565fad9b962.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b55c6aced3e0bf737772562b35e7451",
+    "url": "/WeatherForcast/index.html"
+  },
+  {
+    "revision": "1200728840d5ad4599d5",
+    "url": "/WeatherForcast/static/css/main.c6be150e.chunk.css"
+  },
+  {
+    "revision": "64ca9755fed446d0e28d",
+    "url": "/WeatherForcast/static/js/2.57fdcc49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherForcast/static/js/2.57fdcc49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1200728840d5ad4599d5",
+    "url": "/WeatherForcast/static/js/main.5bac419c.chunk.js"
+  },
+  {
+    "revision": "4f7bf6999a3a10410a3e",
+    "url": "/WeatherForcast/static/js/runtime-main.ef2a4c46.js"
+  }
+]);
